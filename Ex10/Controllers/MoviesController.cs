405f8dbaee3b918516c9d3874190661a23b5d712
s22088 <101318490+s22088@users.noreply.github.com>
2022-06-14 18:20:08 +0200

    Add a view - done

--- Ex10/Controllers/MoviesController.cs
+++ Ex10/Controllers/MoviesController.cs
@@ -5,13 +5,22 @@ namespace Ex10.Controllers
 {
     public class MoviesController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "bbb";
+            return View();
         }
 
-        public string Welcome(string name, int id) {
-            return HtmlEncoder.Default.Encode($"{name} got id {id}");
+        //public string Welcome(string name, int id) {
+        //    return HtmlEncoder.Default.Encode($"{name} got id {id}");
+        //}
+
+        public IActionResult Welcome(string name, int id, int numTimes = 3)
+        {
+            ViewData["Message"] = $"Hello {name}, id {id}";
+            ViewData["times"] = numTimes;
+
+            return View();
         }
+
     }
 }
