629de33e1074cb6bfe5d524462623480f09026da
s22088 <101318490+s22088@users.noreply.github.com>
2022-06-14 17:15:57 +0200

    initial commit

---
+++ Ex10/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ex10.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public string Index()
+        {
+            return "bbb";
+        }
+
+        public string Welcome() {
+            return "zzz";
+        }
+    }
+}
