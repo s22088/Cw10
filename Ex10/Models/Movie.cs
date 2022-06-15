f5a524a027ad489b63dc7ae8644dc2b0951c3ad6
s22088 <101318490+s22088@users.noreply.github.com>
2022-06-15 15:11:32 +0200

    Add validation - done

--- Ex10/Models/Movie.cs
+++ Ex10/Models/Movie.cs
@@ -8,15 +8,24 @@ namespace Ex10.Models
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
-        //[Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 100)]
         public double Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 }
